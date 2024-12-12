7567463d1c10205534e6db9182cae01e70e3c370
msbazzi <msbazzi@stanford.edu>
2024-12-12 12:48:12 -0800

    Adding some simple non uniform thickness

--- Sheep/create_mesh_wall.py
+++ Sheep/create_mesh_wall.py
@@ -134,6 +134,137 @@ def plot_points(points):
     plotter = pv.Plotter()
     plotter.add_mesh(point_cloud, color="blue", point_size=5, render_points_as_spheres=True)
     plotter.show()
+def get_axial_positions(polydata):
+    num_points = polydata.GetNumberOfPoints()
+    axial_positions = np.zeros(num_points)
+    
+    for i in range(num_points):
+        point = polydata.GetPoint(i)
+        axial_positions[i] = point[2]  # Assuming the z-coordinate is the axial position
+
+    return axial_positions
+
+def generate_prismatic_mesh_vtu_nonuniform_thickness(polydata, normals, thickness_vector, radial_layers, output_file):
+    # Create an array to store the new points
+    axial_positions = get_axial_positions(polydata)
+
+    num_points = polydata.GetNumberOfPoints()
+    all_points = np.zeros(((radial_layers+1)*num_points, 7))
+    thickness_values = np.interp(axial_positions, np.linspace(axial_positions.min(), axial_positions.max(), len(thickness_vector)), thickness_vector)
+    
+    ids = np.zeros(((radial_layers+1)*num_points),  dtype=int)
+    prism_connectivity = []
+    global_id_counter = 0
+    inner_ids = np.zeros(((radial_layers+1)*num_points), dtype=int)
+    outer_ids = np.zeros(((radial_layers+1)*num_points), dtype=int)
+
+    for i in range(num_points):
+
+        normal = normals.GetTuple(i)
+        global_id_array = polydata.GetPointData().GetArray("GlobalNodeID").GetValue(i)
+        points_coordinates = polydata.GetPoint(i)
+        all_points[i,0]= global_id_array
+        all_points[i,1] = points_coordinates[0]
+        all_points[i,2] = points_coordinates[1]
+        all_points[i,3] = points_coordinates[2]
+        all_points[i,4] = normal[0]
+        all_points[i,5] = normal[1]
+        all_points[i,6] = normal[2]
+        inner_ids[i] = 1
+        ids[i] = 1
+
+
+    num_cells = polydata.GetNumberOfCells()
+    cell_point_ids = [global_id_counter + i for i in range(num_points)]
+    
+    # Generate new points for each radial layer
+   
+    for i in range(radial_layers):
+        c = i+1
+        factor = c / (radial_layers)  # Scale factor for radial layers
+        
+
+        for j in range(num_points):
+            thickness = thickness_values[j] * factor
+            point = polydata.GetPoint(j)
+            normal = normals.GetTuple(j)
+
+            # Calculate the offset point
+            xPt = point[0] + factor * thickness * (normal[0])
+            yPt = point[1] + factor * thickness * (normal[1])
+            zPt = point[2] + factor * thickness * (normal[2])
+            
+            all_points[num_points*c+j,0] = all_points[num_points*i+j,0]+num_points*c
+            all_points[num_points*c+j,1] = xPt
+            all_points[num_points*c+j,2] = yPt
+            all_points[num_points*c+j,3] = zPt
+            all_points[num_points*c+j,4] = normal[0]
+            all_points[num_points*c+j,5] = normal[1]
+            all_points[num_points*c+j,6] = normal[2]
+        
+            if i == radial_layers - 1:
+                outer_ids[num_points*c+j] = 1
+                ids[num_points*c+j] = 2
+    
+    # Generate connectivity for prismatic cells
+    for i in range(num_cells):
+        
+        cell = polydata.GetCell(i)
+        num_cell_points = cell.GetNumberOfPoints()
+        cell_point_ids = [cell.GetPointId(j) for j in range(num_cell_points)]
+
+        for j in range(radial_layers):
+            base_layer = j * num_points
+            next_layer = (j + 1) * num_points
+
+            # Create prismatic connectivity
+            if num_cell_points==3: # Triangular cells
+                p0 = cell_point_ids[0] + base_layer
+                p1 = cell_point_ids[1] + base_layer
+                p2 = cell_point_ids[2] + base_layer
+
+                p3 = cell_point_ids[0] + next_layer
+                p4 = cell_point_ids[1] + next_layer
+                p5 = cell_point_ids[2] + next_layer
+                # Add a prism cell: 6 points per cell
+                prism_connectivity.append([p0, p1, p2, p3, p4, p5])
+             
+            elif len(num_cell_points)==4: # Quadrilateral cells
+                print("Quadrilateral cells is not implemented yet")              
+                    # Convert all_points to a NumPy array
+    
+    
+   
+    # Convert points to VTK format
+    all_points_coordinates  = all_points[:,1:4]
+    #plot_points(all_points_coordinates)
+    vtk_points = vtk.vtkPoints()
+    for point in all_points_coordinates:
+        vtk_points.InsertNextPoint(point)
+
+    unstructured_grid = vtk.vtkUnstructuredGrid()
+    unstructured_grid.SetPoints(vtk_points)
+
+    # Add prism cells to the unstructured grid
+    for prism in prism_connectivity:
+        vtk_cell = vtk.vtkIdList()
+        for pid in prism:
+            vtk_cell.InsertNextId(pid)
+        unstructured_grid.InsertNextCell(vtk.VTK_WEDGE, vtk_cell)
+    # Write to VTU file
+    writer = vtk.vtkXMLUnstructuredGridWriter()
+    writer.SetFileName(output_file)
+    writer.SetInputData(unstructured_grid)
+    writer.Write()
+
+    # vol_py= pv.wrap(unstructured_grid)
+    # plotter = pv.Plotter()
+    # plotter.add_mesh(vol_py, scalars="OuterID", show_edges=True)
+    # plotter.show()
+    
+    print("VTU file written to: ", output_file)
+
+    return unstructured_grid, all_points
 
 def generate_prismatic_mesh_vtu(polydata, normals, thickness, radial_layers, output_file):
     # Create an array to store the new points
@@ -279,29 +410,14 @@ def saveSolid(vol,surf,output_dir):
         proximal = thresholdModelNew(surf, "ModelFaceID",0.5,1.5)
         proximal_polydata = convert_to_polydata(proximal)  # Convert to vtkPolyData
         save_data(output_dir + '/mesh-surfaces/wall_inlet.vtp',proximal_polydata)
-        if proximal_polydata.GetNumberOfPoints() == 0:
-            print("No inlet surface found.")
-            return
-        else:
-            print("Inlet surface saved to: ", output_dir + '/mesh-surfaces/wall_inlet.vtp')
 
         inner = thresholdModelNew(surf, 'ModelFaceID',1.5,2.5)
         inner_polydata = convert_to_polydata(inner)
         save_data(output_dir + '/mesh-surfaces/wall_inner.vtp',inner_polydata)
-        if inner_polydata.GetNumberOfPoints() == 0:
-            print("No inner surface found.")
-            return
-        else:
-            print("Inner surface saved to: ", output_dir + '/mesh-surfaces/wall_inner.vtp')
 
         distal = thresholdModelNew(surf, "ModelFaceID", 2.5, 3.5)
         distal_polydata = convert_to_polydata(distal)
         save_data(output_dir + '/mesh-surfaces/wall_outlet.vtp', distal_polydata)
-        if distal_polydata.GetNumberOfPoints() == 0:
-            print("No outlet surface found.")
-            return
-        else:
-            print("Outlet surface saved to: ", output_dir + '/mesh-surfaces/wall_outlet.vtp')   
 
         outer = thresholdModelNew(surf, "ModelFaceID", 3.5, 4.5)
         outer_polydata = convert_to_polydata(outer)
@@ -335,19 +451,25 @@ def run_script_with_sv(python_sv,extract_faces_script, faces_file, output_dir):
         print("Error Output:\n", e.stderr)
 
 def main():
+
+    current_directory = os.getcwd()
     python_sv = "/home/bazzi/repo/SimVascular-Ubuntu-20-2023-05/data/usr/local/sv/simvascular/2023-03-27/simvascular"
-    extract_faces_script = "/home/bazzi/TEVG/Prismatic_mesh/Sheep/get_faces_with_sv.py"
+    extract_faces_script = current_directory + "/get_faces_with_sv.py"
     
-    surface = "/home/bazzi/TEVG/Prismatic_mesh/Sheep/lumen-mesh-complete/mesh-surfaces/lumen_wall.vtp"
-    output_file="/home/bazzi/TEVG/Prismatic_mesh/Sheep/solid-mesh-complete/wall.vtu"
-    faces_file = "/home/bazzi/TEVG/Prismatic_mesh/Sheep/solid-mesh-complete/wall_faces.vtp"
-    output_dir = "/home/bazzi/TEVG/Prismatic_mesh/Sheep/solid-mesh-complete"
+    surface = current_directory + "/lumen-mesh-complete/mesh-surfaces/lumen_wall.vtp"
+    output_file = current_directory + "/solid-mesh-complete/wall.vtu"
+    faces_file = current_directory +  "/solid-mesh-complete/wall_faces.vtp"
+    output_dir = current_directory + "/solid-mesh-complete"
     
+    radial_layers = 4
+    thickness_vector = [7e-1, 2e-1, .5e-1, 2e-1, 3e-1, 5e-1]
+
     #extrat the normals to each point in the surface and read the face mesh 
     polydata, normals = point_normals(surface)
     
     # use the normals to generate a prismatic mesh starting from the surface mesh (polydata) return the volume mesh (vol)
-    vol, all_points = generate_prismatic_mesh_vtu(polydata, normals, 0.1, 4, output_file)
+    #vol, all_points = generate_prismatic_mesh_vtu(polydata, normals, 0.1, radial_layers, output_file)
+    vol, all_points =  generate_prismatic_mesh_vtu_nonuniform_thickness(polydata, normals, thickness_vector, radial_layers, output_file)
 
     # Add GlobalNodeID and GlobalElementID to the volume mesh
     global_ids = all_points[:,0]
